feb1f87fabd7588f43616185be0a5ac80f2e9ada
Perrine <perrine.brianchon@gmail.com>
2020-12-15 15:54:33 +0100

    remove old redlab folder

--- redlab/sidebar.php
+++
@@ -1,8 +0,0 @@
- <!-- Sidebar -->
- <aside class="is-4 container">
-     <?php if (is_active_sidebar('about')) : ?>
-         <?php dynamic_sidebar('about'); ?>
-         </div>
-     <?php endif; ?>
- </aside>
- <!-- Fin sidebar -->
