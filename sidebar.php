5e4ddda298b79230a46c5a7db28a64d3e636fc18
Jeremy-76 <jeremy.fortin@hotmail.fr>
2020-12-16 14:18:00 +0100

    Travail sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
  <!-- Sidebar -->
  <aside class="col-md-4 blog-sidebar">
-                <div class="p-4 mb-3">
+                <div class="p-4 mb-3"> 
                     <h4>Catégories</h4>
                             <ul class="mb-0 ml-3 sidebar-link">
                                 <?php 
