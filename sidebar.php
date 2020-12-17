e3cfc931ed91f329484f4dd62dacd7678e96c252
Léo Leclerc <58265578+Hopust-design@users.noreply.github.com>
2020-12-17 09:55:04 +0100

    cleaning

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,5 @@
  <!-- Sidebar -->
  <aside class="is-4 container">
- <?php get_search_form(); ?>
      <?php if (is_active_sidebar('about')) : ?>
          <?php dynamic_sidebar('about'); ?>
          </div>
