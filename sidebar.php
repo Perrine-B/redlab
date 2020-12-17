715197454d70acd91e097839b68b5b6ee39f2e72
Jeremy-76 <jeremy.fortin@hotmail.fr>
2020-12-17 14:17:34 +0100

    début de page home

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
  <!-- Sidebar -->
- <aside class="is-4 container">
+ <aside class="is-4 container m-5">
      <?php if (is_active_sidebar('about')) : ?>
          <?php dynamic_sidebar('about'); ?>
          </div>
