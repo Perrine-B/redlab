44a90c33668e3e628b1b40a3d7ab52cf7f88e67e
Perrine <perrine.brianchon@gmail.com>
2020-12-17 09:39:46 +0100

    cleaning code

--- single.php
+++ single.php
@@ -2,12 +2,9 @@
 <!-- The loop -->
 <? if (have_posts()) : ?>
 <? while (have_posts()) : the_post(); ?>
-
-
 <figure class=" article-hero image is-3by1">
-  <img class="is-fullwidth" src=" http://redlab.local/wp-content/uploads/2020/12/photo-1488229297570-58520851e868.jpg">
+    <img class="is-fullwidth" src=" http://redlab.local/wp-content/uploads/2020/12/photo-1488229297570-58520851e868.jpg">
 </figure>
-
 <section class='article'>
     <?php
     if (function_exists('yoast_breadcrumb')) {
@@ -17,22 +14,15 @@
     <h1 class='title'>
         <? the_title()?>
     </h1>
-
-    <!-- /**
-     TODO : Trouver pourquoi author link ne marche pas 
-      */ -->
     <? $authorId = get_the_author_ID()?>
     <div class='article-info'>
-    <h2 class="subtitle"> <?php the_category() ?> </h2>
-    <h3><?php the_date() ?></h3>
-    <h3><?php the_author_posts_link(); ?></h3>  
+        <h2 class="subtitle"> <?php the_category() ?> </h2>
+        <h3><?php the_date() ?></h3>
+        <h3><?php the_author_posts_link(); ?></h3>
     </div>
-    
-
     <div class='container'>
         <? the_content() ?>
     </div>
-
 </section>
 <hr class="solid">
 <section class='article'>
@@ -40,5 +30,4 @@
 </section>
 <? endwhile; ?>
 <? endif; ?>
-
 <?php get_footer() ?>
