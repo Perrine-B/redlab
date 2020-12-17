9b607f0ef2bd96ee97e453f095351a23fdfab93c
Léo Leclerc <58265578+Hopust-design@users.noreply.github.com>
2020-12-17 09:30:52 +0100

    changing for searchform inside content on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,27 +2,13 @@ const app = {
 
     init: function() {
         // Allows mobile menu opening
+
         const burger = document.querySelector('.burger');
         const nav = document.querySelector('#' + burger.dataset.target);
 
         burger.addEventListener('click', function() {
             burger.classList.toggle('is-active');
             nav.classList.toggle('is-active');
-        });
-
-        // Allows search menu opening
-        const dropdown = document.querySelector('#dropdown');
-        const close = document.querySelector('.delete');
-
-        dropdown.addEventListener('click', function(event) {
-            dropdown.classList.add('is-active');
-            event.stopPropagation();
-            console.log("STOP.");
-            event.preventDefault();
-        });
-        close.addEventListener('click', function() {
-            dropdown.classList.remove('is-active');
-            console.log("Oui.");
         });
 
     }
